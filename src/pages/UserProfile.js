cc2c2df4376c255e898b96d7fea548de977fab71
Abiud-lang <abiud.kiprotich@student.moringaschool.com>
2024-08-18 11:24:22 +0300

    done

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,27 +1,18 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useOutletContext, useParams } from 'react-router-dom';
 
 function UserProfile() {
-  const [user, setUser] = useState({});
   const params = useParams();
-  const userId = params.id;
-
-  useEffect(() =>{
-    fetch(`http://localhost:4000/users/${userId}`)
-    .then(r => r.json())
-    .then(data => setUser(data))
-    .catch(error => console.error(error));
-  }, [userId]);
-
-  if(!user.name){
+  const users = useOutletContext();
+  const user = users.find((user) => user.id === parseInt(params.id));
+  if (!user) {
     return <h1>Loading...</h1>;
-  };
+  }
 
-  return(  
-      <aside>
-        <h1>{user.name}</h1>
-      </aside>
+  return (
+    <aside>
+      <h1>{user.name}</h1>
+    </aside>
   );
-};
+}
 
 export default UserProfile;
